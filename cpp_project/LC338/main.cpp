43d76b151b0f66a6c05fa85acc88f1864ff2e745
Sergey Yatsuk <sergeyy@cqg.com>
2016-10-10 18:21:14 +0300

    Move My progress outside

---
+++ cpp_project/LC338/main.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/counting-bits/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+   vector<int> countBits(int num) {
+
+   }
+};
+int main()
+{
+   Solution s;
+   cout << s.reverseString("abc").c_str() << endl;
+   return 0;
+}
---
+++ cpp_project/LC70/main.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/climbing-stairs/
+
+#include <iostream>
+#include "climbing-stairs.cpp"
+
+using namespace std;
+
+int main()
+{
+   Solution s;
+   cout << s.climbStairs(3) << endl;
+   return 0;
+}

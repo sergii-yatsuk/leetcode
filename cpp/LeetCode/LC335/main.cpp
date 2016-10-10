bebf52ec500180ee71e04e66f37b5a947bf2fb31
Sergey Yatsuk <sergeyy@cqg.com>
2016-10-10 16:33:46 +0300

    LC334

---
+++ cpp/LeetCode/LC335/main.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/self-crossing/
+
+#include <iostream>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+   bool isSelfCrossing(vector<int>& x) {
+
+   }
+};
+
+int main()
+{
+   Solution s;
+   std::vector<int> vec;
+   int a[] = { 1, 2, 3, 4, 5 };
+   vec.assign(a, a + 5);
+   cout << s.isSelfCrossing(vec);
+   return 0;
+}

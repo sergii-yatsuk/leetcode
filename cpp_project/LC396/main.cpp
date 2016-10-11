47bfb89920519b9bf27ed36cab864601abfa4636
Sergey Yatsuk <sergeyy@cqg.com>
2016-10-11 11:47:23 +0300

    LC396

---
+++ cpp_project/LC396/main.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/rotate-function/
+#include <iostream>
+#include "rotate-function.cpp"
+
+int main()
+{
+   Solution s;
+   std::vector<int> vec;
+   int a[] = { 4, 3, 2, 6};
+   vec.assign(a, a + 4);
+   cout << s.maxRotateFunction(vec);
+   return 0;
+}

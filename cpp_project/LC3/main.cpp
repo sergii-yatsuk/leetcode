746b21f2064216271abf8c7ac82f1f8ea8cfa4d6
tifon <yatsuksergey@gmail.com>
2016-11-07 22:28:26 +0200

    LC3

---
+++ cpp_project/LC3/main.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+#include <iostream>
+#include "longest-substring-without-repeating-characters.h"
+
+using namespace std;
+
+int main()
+{
+   Solution s;
+   cout << s.lengthOfLongestSubstring("abcabcbb") << endl;
+   return 0;
+}

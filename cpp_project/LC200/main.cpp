659714bb8a08a54508ef778064670bc4e0ce6e08
tifon <yatsuksergey@gmail.com>
2016-11-12 01:45:10 +0200

    LC200 proj

---
+++ cpp_project/LC200/main.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/number-of-islands/
+
+#include "number-of-islands.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+   Solution s;
+   vector<vector<char>> grid;
+   cout << s.numIslands(grid);
+   return 0;
+}

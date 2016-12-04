e27d39e1cb3d6fec20a15b22ad185c8331ee386e
tifon <yatsuksergey@gmail.com>
2016-12-04 12:44:55 +0200

    LC162M

---
+++ cpp/find-peak-element.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/find-peak-element/
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        if (nums.size()==1) return 0;
+        int l = 0;
+        int r = nums.size()-1;
+        
+        while (l<r) {
+            int mid = (l+r)/2;
+            
+            if (nums[mid] < nums[mid+1]) {
+                l = mid +1;
+            }
+            else {
+                r = mid;
+            }
+
+        }
+
+        return l;
+    }
+};

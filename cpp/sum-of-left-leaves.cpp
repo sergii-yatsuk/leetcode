1350e370dc3d1c93d53f9dd797f88b1d8ee0c359
Sergey Yatsuk <sergeyy@cqg.com>
2016-12-09 13:05:57 +0200

    LC404E

--- cpp/sum-of-left-leaves.cpp
+++ cpp/sum-of-left-leaves.cpp
@@ -1,41 +1,23 @@
-// https://leetcode.com/problems/add-strings/
-
+// https://leetcode.com/problems/sum-of-left-leaves/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    string addStrings(string num1, string num2) {
-        // 123
-        // 1234
+    int sumOfLeftLeaves(TreeNode* root) {
+        return sumOfLeftRec(root, false);
+    }
+    
+    int sumOfLeftRec(TreeNode* root, bool isLeft) {
+        if (!root) return 0;
+        if (root->left == NULL && root->right == NULL && isLeft) return root->val;
+        return sumOfLeftRec(root->left, true) + sumOfLeftRec(root->right, false);
         
-        const size_t num1size = num1.size();
-        const size_t num2size = num2.size();
-        
-        int num1i = num1size-1;
-        int num2i = num2size-1;
-        
-        short prev = 0;
-        string result="";
-        
-        while (num1i>=0 || num2i>=0) {
-            
-            short num1is = 0;
-            short num2is = 0;
-            
-            if (num1i>=0) num1is = num1[num1i]-'0';
-            if (num2i>=0) num2is = num2[num2i]-'0';
-            
-            short su = num1is + num2is + prev;
-            
-            result += char('0'+su%10);
-            prev = su/10;
-            --num1i;
-            --num2i;
-        }
-        if (prev > 0) {
-            result += char('0'+prev);
-        }
-        
-        
-        reverse(result.begin(), result.end());
-        return result;
     }
 };

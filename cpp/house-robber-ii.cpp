9a67896e1de2c218ad83c8cd232db074638e30e2
Sergey Yatsuk <sergeyy@cqg.com>
2016-12-27 14:59:08 +0200

    LC213M

--- cpp/house-robber-ii.cpp
+++ cpp/house-robber-ii.cpp
@@ -1,1 +1,33 @@
 // https://leetcode.com/problems/house-robber-ii/
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        if (nums.size() == 1) return nums[0];
+        if (nums.size() == 2) return max(nums[0], nums[1]);
+        
+        vector<int> money(nums.size());
+        money[0] = nums[0];
+        money[1] = max(nums[0], nums[1]);
+        
+        for (int k=2; k<nums.size()-1; ++k)  {
+            money[k] = max(money[k-2]+nums[k], money[k-1]);
+        }
+        
+
+        int max1 = money[nums.size()-2];
+        
+        money[0] = 0;
+        money[1] = nums[1];
+        
+        for (int k=2; k<nums.size(); ++k)  {
+            money[k] = max(money[k-2]+nums[k], money[k-1]);
+        }
+        
+        int max2 = money[nums.size()-1];
+        
+        return max(max1, max2);
+        
+    }
+};

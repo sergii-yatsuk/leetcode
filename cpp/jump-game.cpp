83e4349d6fab027518c811703125d8b2c8bbd8da
Sergey Yatsuk <sergeyy@cqg.com>
2017-03-25 11:02:43 +0200

    LC55M

---
+++ cpp/jump-game.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/jump-game
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int last=nums.size()-1;
+        
+        for (int i = nums.size()-1; i>=0; --i) {
+            if (nums[i]+i >= last) {
+                last = i;
+            }
+        }
+        
+        return last == 0;
+        
+    }
+};
